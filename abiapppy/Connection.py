e7db54f16c9873592de9a796612220db8222861a
Raphael Jacob <r.jacob2002@gmail.com>
2018-11-26 12:26:59 +0100

    Add simple init of the connection

---
+++ abiapppy/Connection.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+from bs4 import BeautifulSoup
+from .Session import Session
+class Connection:
+
+    def __init__(self, url, email, password, verify=True):
+        self.url = URL(url)
+        self.session = Session()
+
+        @self.session.addCallAfter(self)
+        def getToken(self, req):
+            try:
+                self.csrfmiddlewaretoken = BeautifulSoup(
+                    req.text, features='lxml').find('input').get('value')
+            except AttributeError:
+                pass
+
+        @self.session.addCallBefore(self)
+        def addToken(self, args, kwargs):
+            if 'data' in kwargs:
+                kwargs['data']['csrfmiddlewaretoken'] = \
+                    self.csrfmiddlewaretoken
+            return args, kwargs
+
+        self.session.verify = verify
+        self.csrfmiddlewaretoken = ''
