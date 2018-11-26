e49555aeac3c79b7eabc42ab115f4833b36525f3
Raphael Jacob <r.jacob2002@gmail.com>
2018-11-26 12:24:15 +0100

    introduce package abiapppy

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='abiapppy',
+      description='A simple API for abiapp.net',
+      version='0.0.1',
+      author='Raphael Jacob',
+      author_email='r.jacob2002@gmail.com',
+      url='https://github.com/ski7777/abiapppy',
+      license='GPLv3',
+      packages=find_packages()
+      )
